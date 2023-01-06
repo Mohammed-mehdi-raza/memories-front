615d6ca02071adfe0c3dcf70606c5d0384ffddcf
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2023-01-06 14:00:58 +0530

    some changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import {Container} from '@material-ui/core';
+import Home from './components/Home/Home.js';
+import Auth from './components/Auth/Auth.js';
+import Navbar from './components/Navbar/Navbar.js';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+const App =()=>{
+  return (
+      <BrowserRouter>
+        <Container maxidth='lg'> 
+          <Navbar/>
+          <Routes>
+            <Route path="/" exact element={<Home/>}/>
+            <Route path="/auth" exact element={<Auth/>}/>
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    );
+}
+
+export default App;
